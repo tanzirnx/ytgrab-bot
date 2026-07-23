3309c1a8fdbc9d2d6384d88f0dcd973aa4f9a641
Tanzir TN <tanziruddin56@gmail.com>
2026-07-23 13:03:05 +0600

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,13 @@
 from services.downloader import Downloader, downloader
 from services.queue_manager import QueueManager, queue_manager
 from services.file_manager import FileManager, file_manager
+from services.metadata_service import MetadataService, metadata_service
+from services.sponsorblock_service import SponsorBlockService, sponsorblock_service
 
-__all__ = ["Downloader", "downloader", "QueueManager", "queue_manager", "FileManager", "file_manager"]
+__all__ = [
+    "Downloader", "downloader",
+    "QueueManager", "queue_manager",
+    "FileManager", "file_manager",
+    "MetadataService", "metadata_service",
+    "SponsorBlockService", "sponsorblock_service",
+]
