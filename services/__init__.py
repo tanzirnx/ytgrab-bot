cbee0b9ac37c7e05cc7e21042f01dbf408ccffaf
Tanzir TN <tanziruddin56@gmail.com>
2026-07-23 12:53:15 +0600

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,3 +1,5 @@
-from services.downloader import Downloader
+from services.downloader import Downloader, downloader
+from services.queue_manager import QueueManager, queue_manager
+from services.file_manager import FileManager, file_manager
 
-__all__ = ["Downloader"]
+__all__ = ["Downloader", "downloader", "QueueManager", "queue_manager", "FileManager", "file_manager"]
